77eae9914d8a278130a763e977c268416a83c198
Amassana Serge Junior <amassanasergejunior@gmail.com>
2024-05-24 20:53:02 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup  from './Signup'
+import {BroswerRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+ 
+  return (
+  <div>
+   <Signup/>
+  </div>
+  )
+}
+
+export default App
